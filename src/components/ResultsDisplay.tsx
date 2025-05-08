ed569d3f600e0e17be4390efb41ebd5c2a7c4d38
kkauf <hello@kkauf.com>
2025-05-08 12:35:58 +0200

    refactor: comment out unused getTimeAgo function in ResultsDisplay component

--- src/components/ResultsDisplay.tsx
+++ src/components/ResultsDisplay.tsx
@@ -110,33 +110,33 @@ export default function ResultsDisplay({
   );
 }
 
-function getTimeAgo(date: Date): string {
-  const now = new Date();
-  let diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+// function getTimeAgo(date: Date): string {
+//   const now = new Date();
+//   let diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
 
-  if (diffInSeconds < 0) {
-    // Timestamp is in the future
-    return "just now";
-  }
+//   if (diffInSeconds < 0) {
+//     // Timestamp is in the future
+//     return "just now";
+//   }
 
-  if (diffInSeconds < 5) {
-    return "just now";
-  }
+//   if (diffInSeconds < 5) {
+//     return "just now";
+//   }
 
-  if (diffInSeconds < 60) {
-    return `${diffInSeconds} seconds ago`;
-  }
+//   if (diffInSeconds < 60) {
+//     return `${diffInSeconds} seconds ago`;
+//   }
 
-  const diffInMinutes = Math.floor(diffInSeconds / 60);
-  if (diffInMinutes < 60) {
-    return `${diffInMinutes} minute${diffInMinutes !== 1 ? "s" : ""} ago`;
-  }
+//   const diffInMinutes = Math.floor(diffInSeconds / 60);
+//   if (diffInMinutes < 60) {
+//     return `${diffInMinutes} minute${diffInMinutes !== 1 ? "s" : ""} ago`;
+//   }
 
-  const diffInHours = Math.floor(diffInMinutes / 60);
-  if (diffInHours < 24) {
-    return `${diffInHours} hour${diffInHours !== 1 ? "s" : ""} ago`;
-  }
+//   const diffInHours = Math.floor(diffInMinutes / 60);
+//   if (diffInHours < 24) {
+//     return `${diffInHours} hour${diffInHours !== 1 ? "s" : ""} ago`;
+//   }
 
-  const diffInDays = Math.floor(diffInHours / 24);
-  return `${diffInDays} day${diffInDays !== 1 ? "s" : ""} ago`;
-}
+//   const diffInDays = Math.floor(diffInHours / 24);
+//   return `${diffInDays} day${diffInDays !== 1 ? "s" : ""} ago`;
+// }
